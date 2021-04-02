07517997fe978aff9e4579686360ec04b2e290cf
Josue Narro <josue.narro.a@gmail.com>
2021-04-02 02:29:49 -0500

    Add function to show product cards

--- script.js
+++ script.js
@@ -7,9 +7,35 @@ async function getProducts() {
         const response = await fetch(`${BASE_URL}/products`)
         const data = await response.json()
         console.log(data)
+        showProducts(data)
     } catch (error) {
         // Catch errors
     }
+}
+
+function showProducts(products) {
+    products.forEach((item) => {
+        const product = document.createElement('div')
+        product.classList.add('product', 'col')
+        product.innerHTML = `
+            <article class="card h-100">
+                <img src="${item.url_image}" class="card-img-top" alt="${item.name}" />
+                <div class="card-body">
+                    <h5 class="card-title text-center">${item.name}</h5>
+                    <div class="d-flex justify-content-between">
+                        <p class="card-text">${item.price}</p>
+                        <button
+                            type="button"
+                            class="btn btn-secondary btn-sm"
+                        >
+                            <i class="icon bi bi-cart-plus"></i>
+                        </button>
+                    </div>
+                </div>
+            </article>
+        `
+        productsContainer.append(product)
+    })
 }
 
 getProducts()
