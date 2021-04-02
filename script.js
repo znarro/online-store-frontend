d564d43cfb7efe27d19bd7e4060b63cf533ccef8
Josue Narro <josue.narro.a@gmail.com>
2021-04-02 13:18:08 -0500

    Add message when product not found

--- script.js
+++ script.js
@@ -25,36 +25,34 @@ function showProducts(products) {
       const product = document.createElement('div')
       product.classList.add('product', 'col')
       product.innerHTML = `
-                <article class="card h-100">
-                    <img src="${
-                      item.url_image || '/image-not-found.png'
-                    }" class="card-img-top" alt="${item.name}" />
-                    <div class="card-body">
-                        <h5 class="card-title text-center">${item.name.toUpperCase()}</h5>
-                        <div class="d-flex justify-content-between align-items-center">
-                            <p class="card-text">${new Intl.NumberFormat(
-                              'en-US',
-                              {
-                                style: 'currency',
-                                currency: 'USD',
-                              }
-                            ).format(item.price)}</p>
-                            <button
-                                type="button"
-                                class="btn btn-secondary btn-sm"
-                            >
-                                <i class="icon bi bi-cart-plus"></i>
-                            </button>
-                        </div>
-                    </div>
-                </article>
-            `
+        <article class="card h-100">
+          <img src="${
+            item.url_image || '/image-not-found.png'
+          }" class="card-img-top" alt="${item.name}" />
+          <div class="card-body">
+            <h5 class="card-title text-center">${item.name.toUpperCase()}</h5>
+            <div class="d-flex justify-content-between align-items-center">
+              <p class="card-text">${new Intl.NumberFormat('en-US', {
+                style: 'currency',
+                currency: 'USD',
+              }).format(item.price)}</p>
+              <button
+                type="button"
+                class="btn btn-secondary btn-sm"
+              >
+                <i class="icon bi bi-cart-plus"></i>
+              </button>
+            </div>
+          </div>
+        </article>
+      `
       productsContainer.append(product)
     })
   } else {
-    const productNotFound = document.createElement('p')
+    const productNotFound = document.createElement('h2')
     productNotFound.classList.add('not-found')
-    productNotFound.innerHTML = 'Temporarily out of stock'
+    productNotFound.innerHTML = 'Temporarily out of stock 😔'
+    productsContainer.append(productNotFound)
   }
 }
 
@@ -62,7 +60,6 @@ form.addEventListener('submit', (e) => {
   e.preventDefault()
   let { query } = e.target.elements
   query = query.value.toLowerCase()
-
   getProducts(BASE_URL + `?q=${query}`)
 })
 
