5010a2fa7bf2d524d27ac0fd56becaa86a5be56c
Josue Narro <josue.narro.a@gmail.com>
2021-04-02 12:58:17 -0500

    Add eslint-config-prettier

--- script.js
+++ script.js
@@ -7,59 +7,68 @@ let BASE_URL = 'http://127.0.0.1:3000/products'
 getProducts(BASE_URL)
 
 async function getProducts(url) {
-    try {
-        const response = await fetch(url)
-        const data = await response.json()
-        showProducts(data)
-    } catch (error) {
-        // Catch errors
-    }
+  try {
+    const response = await fetch(url)
+    const data = await response.json()
+    showProducts(data)
+  } catch (error) {
+    // Catch errors
+  }
 }
 
 function showProducts(products) {
-    // Clear products first
-    productsContainer.innerHTML = ''
+  // Clear products first
+  productsContainer.innerHTML = ''
 
+  if (products.length > 0) {
     products.forEach((item) => {
-        const product = document.createElement('div')
-        product.classList.add('product', 'col')
-        product.innerHTML = `
-            <article class="card h-100">
-                <img src="${
-                    item.url_image || '/image-not-found.png'
-                }" class="card-img-top" alt="${item.name}" />
-                <div class="card-body">
-                    <h5 class="card-title text-center">${item.name.toUpperCase()}</h5>
-                    <div class="d-flex justify-content-between align-items-center">
-                        <p class="card-text">${new Intl.NumberFormat('en-US', {
-                            style: 'currency',
-                            currency: 'USD',
-                        }).format(item.price)}</p>
-                        <button
-                            type="button"
-                            class="btn btn-secondary btn-sm"
-                        >
-                            <i class="icon bi bi-cart-plus"></i>
-                        </button>
+      const product = document.createElement('div')
+      product.classList.add('product', 'col')
+      product.innerHTML = `
+                <article class="card h-100">
+                    <img src="${
+                      item.url_image || '/image-not-found.png'
+                    }" class="card-img-top" alt="${item.name}" />
+                    <div class="card-body">
+                        <h5 class="card-title text-center">${item.name.toUpperCase()}</h5>
+                        <div class="d-flex justify-content-between align-items-center">
+                            <p class="card-text">${new Intl.NumberFormat(
+                              'en-US',
+                              {
+                                style: 'currency',
+                                currency: 'USD',
+                              }
+                            ).format(item.price)}</p>
+                            <button
+                                type="button"
+                                class="btn btn-secondary btn-sm"
+                            >
+                                <i class="icon bi bi-cart-plus"></i>
+                            </button>
+                        </div>
                     </div>
-                </div>
-            </article>
-        `
-        productsContainer.append(product)
+                </article>
+            `
+      productsContainer.append(product)
     })
+  } else {
+    const productNotFound = document.createElement('p')
+    productNotFound.classList.add('not-found')
+    productNotFound.innerHTML = 'Temporarily out of stock'
+  }
 }
 
 form.addEventListener('submit', (e) => {
-    e.preventDefault()
-    let { query } = e.target.elements
-    query = query.value.toLowerCase()
+  e.preventDefault()
+  let { query } = e.target.elements
+  query = query.value.toLowerCase()
 
-    getProducts(BASE_URL + `?q=${query}`)
+  getProducts(BASE_URL + `?q=${query}`)
 })
 
 // Get all products when clear search input
 form.addEventListener('search', (e) => {
-    if (e.target.value === '') {
-        getProducts(BASE_URL)
-    }
+  if (e.target.value === '') {
+    getProducts(BASE_URL)
+  }
 })
