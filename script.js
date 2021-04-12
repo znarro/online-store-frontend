363691c4a4681ad7994b7fd4769759b321a5340d
Josue Narro <josue.narro.a@gmail.com>
2021-04-12 13:46:26 -0500

    Add categories

--- script.js
+++ script.js
@@ -1,11 +1,15 @@
 const productsContainer = document.getElementById('products')
 const form = document.getElementById('form')
 const loader = document.getElementById('loader')
+const categoriesContainer = document.getElementById('categories')
 
-let BASE_URL = 'https://enigmatic-shelf-95625.herokuapp.com/products'
+// const BASE_URL = 'https://enigmatic-shelf-95625.herokuapp.com/products'
+const BASE_URL = 'http://localhost:3000'
 
 // Initially get all the products
-getProducts(BASE_URL)
+getProducts(BASE_URL + '/products')
+// And all the categories for the dropdown
+getCategories(BASE_URL + '/categories')
 
 async function getProducts(url) {
   try {
@@ -59,16 +63,55 @@ function showProducts(products) {
   }
 }
 
+async function getCategories(url) {
+  try {
+    // Clear categories first
+    categories.innerHTML = ''
+    const response = await fetch(url)
+    const data = await response.json()
+    console.log(data)
+    listCategories(data)
+  } catch (err) {
+    console.error(`${err.name}: ${err.message}`)
+  }
+}
+
+function listCategories(categories) {
+  categories.forEach((category) => {
+    const categoryName = category.name[0].toUpperCase() + category.name.slice(1)
+    const categoryItem = document.createElement('li')
+    categoryItem.addEventListener('click', () => {
+      getProducts(BASE_URL + `/products?category_id=${category.id}`)
+    })
+    categoryItem.innerHTML = `
+      <a class="dropdown-item category" id="category-${category.id}">${categoryName}</a>
+    `
+    categoriesContainer.append(categoryItem)
+  })
+}
+
 form.addEventListener('submit', (e) => {
   e.preventDefault()
   let { query } = e.target.elements
   query = query.value.toLowerCase()
-  getProducts(BASE_URL + `?q=${query}`)
+  getProducts(BASE_URL + `/products?q=${query}`)
 })
 
 // Get all products when clear search input
 form.addEventListener('search', (e) => {
   if (e.target.value === '') {
-    getProducts(BASE_URL)
+    getProducts(BASE_URL + '/products')
   }
 })
+
+// Hacer un fetch de categories
+// obtener los nombres, luego iterarlos
+// por cada uno crear un li para el select
+
+// categories.forEach((category) => {
+//   console.log(category)
+// })
+
+// drop.addEventListener('click', () => {
+//   getProducts(BASE_URL + `products?category_id=3`)
+// })
